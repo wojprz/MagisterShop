257c38aef3caa406b5aa76addfec20b7dbc5bcab
Wojciech Przybyła <wojprz@st.amu.edu.pl>
2021-02-19 00:55:56 +0100

    Dodano comment repository

---
+++ MagisterShop.Domain/Repositories/CommentRepository.cs
@@ -0,0 +1,48 @@
+﻿using MagisterShop.Domain.Contexts;
+using MagisterShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagisterShop.Domain.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly MagisterShopContext _entities;
+
+        public CommentRepository(MagisterShopContext entities)
+        {
+            _entities = entities;
+        }
+
+        public async Task AddAsync(Comment comment)
+        {
+            await _entities.AddAsync(comment);
+            await _entities.SaveChangesAsync();
+        }
+
+        public async Task<Comment> GetAsync(Guid id) => await _entities.Comments.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<IEnumerable<Comment>> GetAllProductComments(Guid productId) => await _entities.Comments.Where(x => x.ProductId == productId).ToListAsync();
+
+        public async Task<IEnumerable<Comment>> GetAllUserComments(Guid userId) => await _entities.Comments.Where(x => x.User.Id == userId).ToListAsync();
+
+        public async Task<IEnumerable<Comment>> GetAllUserComments(string login) => await _entities.Comments.Where(x => x.User.Login == login).ToListAsync();
+
+        public async Task RemoveAsync(Guid commentId)
+        {
+            var comment = await GetAsync(commentId);
+            _entities.Remove(comment);
+            await _entities.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Comment comment)
+        {
+            _entities.Comments.Update(comment);
+            await _entities.SaveChangesAsync();
+        }
+    }
+}
---
+++ MagisterShop.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,19 @@
+﻿using MagisterShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagisterShop.Domain.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task AddAsync(Comment comment);
+        Task<Comment> GetAsync(Guid id);
+        Task<IEnumerable<Comment>> GetAllProductComments(Guid productId);
+        Task<IEnumerable<Comment>> GetAllUserComments(Guid userId);
+        Task<IEnumerable<Comment>> GetAllUserComments(string login);
+        Task RemoveAsync(Guid commentId);
+        Task UpdateAsync(Comment comment);
+    }
+}
